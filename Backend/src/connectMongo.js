20b1d86b308f64d7ef22ae18025e4ed15783c8d8
tanisha <tanisha-email@example.com>
2026-04-22 11:21:32 +0530

    updation

--- Backend/src/connectMongo.js
+++ Backend/src/connectMongo.js
@@ -1,13 +1,18 @@
+
+// Import mongoose library to interact with MongoDB
 const mongoose = require('mongoose');
 
 // Establish connection to MongoDB with specified database name
 async function connectToMongoDB(url) {
     
+    // Connect to MongoDB using the provided URL
+    // The 'dbName' specifies which database to use inside the cluster
     return mongoose.connect(url, {
-        dbName: "anchor",   
+        dbName: "anchor",    // Database name (will be created if it doesn't exist)
     });
 }
 
+// Export the connection function so it can be used in server.js
 module.exports = {
     connectToMongoDB,
 }
